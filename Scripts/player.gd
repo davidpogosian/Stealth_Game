9639a1d16bba66932b9135b9144c158910922555
David <david_pogosian@outlook.com>
2023-01-21 22:35:58 -0500

    branch commit

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -26,7 +26,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump..
+	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
